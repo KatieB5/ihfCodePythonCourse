97ba5c3f19194684afc81ffab37c7ca15d184049
KatieB5 <katie.bickford2@gmail.com>
2020-05-16 15:08:16 +0100

    added section b3 answer for Q4

--- session_3_exercises/section_b3.py
+++ session_3_exercises/section_b3.py
@@ -8,8 +8,12 @@ apple["best_before"] = "03/05/2021"
 apple["Price"] = "0.41"
 
 #4
+apple["is_on_sale"] = True
+print(apple)
+
+#5
 for key in apple:
     print(str(key) + " = " + str(apple[key]))
 
-#5
-del apple["Price"]
+#6
+del apple["is_on_sale"]
