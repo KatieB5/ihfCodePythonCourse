a7f3916fe03682781c92b5a0ed20d88b5d35ca6d
KatieB5 <katie.bickford2@gmail.com>
2020-05-17 18:00:07 +0100

    Added session 4, section A exercises to README and added answers

---
+++ session_4_exercises/section_a4.py
@@ -0,0 +1,28 @@
+#1
+fruits = ["Apples", "Cherries", "Pears", "Pineapple", "Peaches", "Mango"]
+
+print(fruits)
+
+#2
+fruits.append("Grapes")
+
+print(fruits)
+
+#3
+fruits[2] = "Strawberries"
+
+print(fruits)
+
+#4
+del fruits[0]
+
+print(fruits)
+
+#5
+print(len(fruits))
+
+#6
+fruits.sort()
+
+#7
+print(fruits)
