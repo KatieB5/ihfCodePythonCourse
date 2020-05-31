159a0bbc12a1316f2ca3f2e0999a54dd940c158d
KatieB5 <katie.bickford2@gmail.com>
2020-05-31 17:34:47 +0100

    Completed section b exercises for session 6

---
+++ session_6_exercises/section_b6.py
@@ -0,0 +1,88 @@
+# 1
+f = open("register.txt", "a")
+
+name = True
+while name:
+    name = input("What is your name? ")
+    f.write(name + "\n")
+f.close()
+
+# 2
+def is_odd(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+f = open("numbers.txt", "r")
+
+g = open("even.txt", "w")
+for x in f:
+    if is_odd(int(x)):
+        g.write(x)
+g.close()
+
+# 3
+cipher = {
+    "1": "A",
+    "2": "B",
+    "3": "C",
+    "4": "D",
+    "5": "E",
+    "6": "F",
+    "7": "G",
+    "8": "H",
+    "9": "I",
+    "10": "J",
+    "11": "K",
+    "12": "L",
+    "13": "M",
+    "14": "N",
+    "15": "O",
+    "16": "P",
+    "17": "Q",
+    "18": "R",
+    "19": "S",
+    "20": "T",
+    "21": "U",
+    "22": "V",
+    "23": "W",
+    "24": "X",
+    "25": "Y",
+    "26": "Z"
+}
+
+decoded_message = ""
+
+f = open("secret.txt", "r")
+for x in f:
+        cipher_key = x.strip()
+        # decoded_message += cipher[x]
+        print(cipher[cipher_key])
+print(decoded_message)
+
+# 4
+def fake_data_calculator(file):
+    f = open(file, "r")
+    
+    total_nums_in_file = 0
+    total_nums_starting_with_1 = 0
+
+    for x in f:
+        total_nums_in_file += 1
+        if x[0] == "1":
+            total_nums_starting_with_1 += 1
+    
+    leading_digit_probability = ((total_nums_starting_with_1/total_nums_in_file) * 100)
+
+    if int(leading_digit_probability) >= 30:
+        print("For " + file + " leading digit probablity is " + str(leading_digit_probability) + ", so this file probably does not contain fake data")
+    else:
+        print("For " + file + " leading digit probablity is " + str(leading_digit_probability) + ", so this file probably contains fake data")
+
+data = ["accounts_1.txt", "accounts_2.txt", "accounts_3.txt"]
+
+for file in data:
+    fake_data_calculator(file)
+
+
+
